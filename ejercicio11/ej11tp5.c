8985facb541e83a439d27c41a638ec0f1ce63de0
unknown <rodrigozucchini1997@gmail.com>
2023-09-08 10:29:21 -0300

    cambios en los valores

--- ejercicio11/ej11tp5.c
+++ ejercicio11/ej11tp5.c
@@ -4,7 +4,6 @@ sido dividido en 3 zonas, en cada zona hay 4 sucursales y en cada sucursal hay 2
 Ingresar los totales de ventas de cada zona, de cada sucursal y cada vendedor. Utilice un
 arreglo de 3 dimensiones para almacenar las ventas.
 */
-
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
@@ -13,27 +12,24 @@ int main(int argc, char const *argv[])
     int sucursales = 4;
     int vendedores = 2;
     double empresaY[zona][sucursales][vendedores];
-    float totalZona = 0.0;
-    float totalSucursales = 0.0;
+    double ventasY[zona][sucursales][vendedores];
 
-    for(int z = 0; z < zona; z++) 
-    {
-        for (int s = 0; s < sucursales; s++)
-        {
-            for(int v = 0; v < vendedores; v++) 
-            {
-                printf("Ingrese el total de ventas de la zona %d, sucursal %d y el vendedor %d \n", z+1, s+1, v+1);
+        for(int z = 0; z < zona; z++) {  
+        for(int s = 0; s < sucursales; s++) {
+            for(int v = 0; v < vendedores; v++) {
+                printf("Ingrese el total de ventas de la zona %d sucursal %d, y el vendedor %d \n", z+1, s+1, v+1);
                 scanf("%lf", &empresaY[z][s][v]);
-                printf("La zona numero %d , la sucursal numero %d ,del vendedor %d tiene un ingreso total de $ %.2f \n\n\n", z+1, s+1, v+1, empresaY[z][s][v]);
-                totalSucursales += empresaY[z][s][v];
-                totalZona += empresaY[z][s][v];
+                ventasY[z][s][v] = empresaY[z][s][v];
             }
-            printf("EL TOTAL DE LA SUCURSAL %d ES DE %.2f \n\n\n", s+1, totalSucursales);
-            totalSucursales = 0.0;
         }
-        printf("EL TOTAL DE LA ZONA %d ES DE %.2f \n\n\n", z+1, totalZona);
-        totalZona = 0.0;
     }
 
-    return 0;
+    for(int j = 0; j < zona; j++) {
+        for(int k = 0; k < sucursales; k++) {
+            for(int l = 0; l < vendedores; l++) {
+                printf("La zona %d, sucursal numero %d, del vendedor %d tiene un ingreso total de $ %.2f \n\n\n", j+1, k+1, l+1, ventasY[j][k][l]);
+            }
+        }
+    }
+   return 0;
 }
--- ejercicio12/ej12tp5.c
+++ ejercicio12/ej12tp5.c
@@ -11,32 +11,36 @@ int main(int argc, char const *argv[])
     int sucursales = 4;
     int vendedores = 2;
     double empresaY[zona][sucursales][vendedores];
+    float totalGeneral = 0.0;
     float totalZona = 0.0;
     float totalSucursales = 0.0;
-    float totalGeneral = 0.0;
 
-    for(int z = 0; z < zona; z++) 
-    {
-        for (int s = 0; s < sucursales; s++)
-        {
-            for(int v = 0; v < vendedores; v++) 
-            {
-                printf("Ingrese el total de ventas de la zona %d, sucursal %d y el vendedor %d \n", z+1, s+1, v+1);
+        for(int z = 0; z < zona; z++) {  
+            totalZona = 0.0;  
+        for(int s = 0; s < sucursales; s++) {
+            totalSucursales = 0.0;
+            for(int v = 0; v < vendedores; v++) {
+                printf("Ingrese el total de ventas de la zona %d sucursal %d, y el vendedor %d \n", z+1, s+1, v+1);
                 scanf("%lf", &empresaY[z][s][v]);
-                printf("La zona numero %d , la sucursal numero %d ,del vendedor %d tiene un ingreso total de $ %.2f \n\n\n", z+1, s+1, v+1, empresaY[z][s][v]);
+                totalGeneral += empresaY[z][s][v];
                 totalSucursales += empresaY[z][s][v];
                 totalZona += empresaY[z][s][v];
-                totalGeneral += empresaY[z][s][v];
             }
-            printf("EL TOTAL DE LA SUCURSAL %d ES DE %.2f \n\n\n", s+1, totalSucursales);
-            totalSucursales = 0.0;
         }
-        printf("EL TOTAL DE LA ZONA %d ES DE %.2f \n\n\n", z+1, totalZona);
-        totalZona = 0.0;
     }
-    printf("-------------------------------------------------\n");
-    printf("EL TOTAL DE TODAS LAS VENTAS DE TODAS LAS SUCURSALES, ZONAS Y VENDEDORES ES DE $ %.2f \n\n\n", totalGeneral);
-    printf("-------------------------------------------------\n");
+
+    for(int j = 0; j < zona; j++) {
+        for(int k = 0; k < sucursales; k++) {
+            for(int l = 0; l < vendedores; l++) {
+                printf("La zona %d, sucursal numero %d, del vendedor %d tiene un ingreso total de $ %.2f \n\n\n", j+1, k+1, l+1, empresaY[j][k][l]);
+            }
+            printf("EL TOTAL DE ZONA %d LA SUCURSAL %d ES DE %.2f \n\n\n", j+1, k+1, totalSucursales);
+        }
+        printf("EL TOTAL DE LA ZONA %d ES DE %.2f \n\n\n", j+1, totalZona);
+    }
+
+    printf("EL TOTAL VENDIDO DE TODOS LOS VENDEDORES DE TODAS LAS SUCURSALES ES DE $ %.2f \n", totalGeneral);
 
     return 0;
 }
+
